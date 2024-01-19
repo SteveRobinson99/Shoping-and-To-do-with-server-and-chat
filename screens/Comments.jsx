3db40cf3ead711af6d1fb8f75ab09083bcd3bfe6
BigDaddyTarpon <gambiatarpon@yahoo.co.uk>
2024-01-19 09:39:35 +0000

    add basic working file structure, needs basic formatting

---
+++ screens/Comments.jsx
@@ -0,0 +1,15 @@
+import { SafeAreaView, Text, StyleSheet, View, FlatList } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import React, { useState } from "react";
+import ShoppingList from "./ShoppingList";
+
+;
+
+const Comments = () => {
+    
+    return (<View> 
+        <Text>'hello world' from Comments</Text>
+            </View>)
+}
+
+export default Comments
