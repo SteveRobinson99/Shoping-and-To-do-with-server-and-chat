ba02424e8cc784cc69aa7bd397d85dc6268c437d
BigDaddyTarpon <gambiatarpon@yahoo.co.uk>
2024-01-22 07:43:56 +0000

    add nodemon to script for concurrent start of app with metro dev server as well as express server for socket.io

--- server/index.js
+++ server/index.js
@@ -1,33 +1,32 @@
 const express = require("express");
 const app = express();
 const PORT = 4000;
-const http = require('http').createServer(app);
+const http = require("http").createServer(app);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const socketIO = require('socket.io')(http, {
-    cors: {
-        origin: "*" 
-        //= io.connect("http://192.168.0.20:4000")
-    }
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+    //= io.connect("http://192.168.0.20:4000")
+  },
 });
 
+socketIO.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
 
-socketIO.on('connection', (socket) => {
-    console.log(`⚡: ${socket.id} user just connected!`);
-
-    socket.on('disconnect', () => {
-      socket.disconnect()
-      console.log('🔥: A user disconnected');
-    });
+  socket.on("disconnect", () => {
+    socket.disconnect();
+    console.log("🔥: A user disconnected");
+  });
 });
 
 app.get("/api", (req, res) => {
-    res.json({
-        message: "Hello world",
-    });
+  res.json({
+    message: "Hello world",
+  });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on ${PORT}`);
 });
